c703119c2027e8159d8ee7120014e91ce7726cf8
Joe User <joeuser@workstation.demo.monzell.com>
2015-04-02 17:31:06 +0000

    Added mco support

--- templates/webhook.rb
+++ templates/webhook.rb
@@ -35,13 +35,27 @@ end
 
 # Some defines.
 def deployEnv(branchname,cmd_obj)
-  deployCmd = "#{cmd_obj['r10k_cmd']} deploy environment #{branchname} -pv"
+  if cmd_obj['mco'] # Check to see if we are using mco
+    if cmd_obj['mco_user'] # run it as a mco user if specified
+      deployCmd = "/bin/su #{cmd_obj['mco_user']} '#{cmd_obj['mco_cmd']} deploy #{branchname}'"
+    else # if we are running mco as root
+      deployCmd = "#{cmd_obj['mco_user']} #{cmd_obj['mco_cmd']} deploy #{branchname}"
+  else # use deefaults
+    deployCmd = "#{cmd_obj['r10k_cmd']} deploy environment #{branchname} -pv"
+  end
   logger.info("Now running #{deployCmd}")
   `#{deployCmd}`
 end
 
 def deployModule(modulename,cmd_obj)
-  deployCmd = "#{cmd_obj['r10k_cmd']} deploy module #{modulename} -pv"
+  if cmd_obj['mco'] # Check to see if we are using mco
+    if cmd_obj['mco_user'] # run it as a mco user if specified
+      deployCmd = "/bin/su #{cmd_obj['mco_user']} '#{cmd_obj['mco_cmd']} deploy #{modulename}'"
+    else
+      deployCmd = "#{cmd_obj['mco_user']} #{cmd_obj['mco_cmd']} deploy_module #{modulename}"
+  else # use defaults
+    deployCmd = "#{cmd_obj['r10k_cmd']} deploy module #{modulename} -pv"
+  end
   logger.info("Now running #{deployCmd}")
   `#{deployCmd}`
 end
